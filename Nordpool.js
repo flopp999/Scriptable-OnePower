bf1a95ed51e0cfc5882053b82ed8cd519ebb6b85
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-05-22 17:22:08 +0200

    Update Nordpool.js

--- Nordpool.js
+++ Nordpool.js
@@ -4,7 +4,7 @@
 // 📄 License: Personal use only. See LICENSE for details.
 // This script was created by Flopp999
 // Support me with a coffee https://www.buymeacoffee.com/flopp999 
-let version = 0.692
+let version = 0.693
 let area
 let resolution
 let currency

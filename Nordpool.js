9f1d412dce8653572d416698d4d61fbd3f6e4ded
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-05-24 09:42:28 +0200

    Update Nordpool.js

--- Nordpool.js
+++ Nordpool.js
@@ -357,115 +357,7 @@ async function askForExtras() {
   return newCost;
 }
 
- async function PriceStats() {
-  let bottom = listwidget.addStack();
-  // lowest
-  let lowest = bottom.addText(t("lowest"));
-  lowest.font = Font.lightSystemFont(11);
-  lowest.textColor = new Color("#00cf00");
-  bottom.addSpacer(4);
-  let priceLowestRound = Math.round(priceLowest);
-  let lowesttext = bottom.addText(`${priceLowestRound}`);
-  lowesttext.font = Font.lightSystemFont(11);
-  lowesttext.textColor = new Color("#00cf00");
-  bottom.addSpacer();
-  // average
-  let avg = bottom.addText(t("average"));
-  avg.font = Font.lightSystemFont(11);
-  avg.textColor = new Color("#f38");
-  bottom.addSpacer(4);
-  let priceAvgRound = Math.round(priceAvg);
-  let avgtext = bottom.addText(`${priceAvgRound}`);
-  avgtext.font = Font.lightSystemFont(11);
-  avgtext.textColor = new Color("#f38");
-  bottom.addSpacer();
-  // highest
-  let highest = bottom.addText(t("highest"));
-  highest.font = Font.lightSystemFont(11);
-  highest.textColor = new Color("#fa60ff");
-  bottom.addSpacer(4);
-  let priceHighestRound = Math.round(priceHighest);
-  let highesttext = bottom.addText(`${priceHighestRound}`);
-  highesttext.font = Font.lightSystemFont(11);
-  highesttext.textColor = new Color("#fa60ff");
-  }
-
-
-const smallFont = 10;
-const mediumFont = 12;
-const bigFont = 13.5;
-const date = new Date();
-const yyyy = date.getFullYear();
-const mm = String(date.getMonth() + 1).padStart(2, '0'); // month are indexed from 0
-const dd = String(date.getDate()).padStart(2, '0');
-const formattedDate = `${yyyy}-${mm}-${dd}`;
-const hour = date.getHours();
-const minute = date.getMinutes();
-const hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-url = `https://dataportal-api.nordpoolgroup.com/api/DayAheadPriceIndices?date=${formattedDate}&market=DayAhead&indexNames=${area}&currency=${currency}&resolutionInMinutes=${resolution}`;
-const request = new Request(url);
-request.timeoutInterval = 1;
-let response = (await request.loadJSON());
-let updated = response.updatedAt;
-updated = updated.replace(/\.\d+Z$/, '').replace('T', ' ');
-const day = response.deliveryDateCET;
-let prices = response.multiIndexEntries;
-let allValues = [];
-
-for (let i = 0; i < prices.length; i++) {
-  const value = prices[i]["entryPerArea"][`${area}`];
-  allValues.push(String(value/10* (1 + "." + (includevat*vat)) + extras));
-}
-
-let pricesJSON = JSON.parse(JSON.stringify(allValues));
-// Test data for 15 min values
-//pricesJSON = ["53.7", "89.1", "60.2", "97.9", "70.8", "40.3", "48.6", "81.7", "26.4", "73.5","75.1", "39.7", "62.8", "18.5", "92.6", "33.1", "20.7", "11.4", "55.5", "46.9","85.0", "35.6", "79.2", "90.4", "66.7", "67.3", "28.8", "15.3", "99.6", "64.5","38.9", "57.2", "19.8", "71.6", "84.4", "49.5", "14.7", "63.1", "21.6", "44.2","78.5", "37.4", "17.2", "13.8", "12.6", "45.3", "58.6", "43.8", "16.9", "69.2","24.1", "41.6", "50.8", "36.3", "59.9", "95.4", "42.5", "93.7", "61.4", "27.5","47.7", "31.9", "32.8", "25.2", "83.6", "30.5", "74.2", "22.4", "77.1", "29.6","34.7", "52.1", "56.8", "23.3", "86.3", "65.4", "91.2", "68.4", "94.9", "98.5","76.3", "87.5", "88.7", "51.3", "80.1", "82.2", "72.7", "96.8", "87.0", "10.9","10.1", "10.4", "10.7", "11.9", "12.2", "13.5", "14.1", "15.8", "16.4", "17.7"]
-  
-const priceLowest = (Math.min(...pricesJSON.map(Number)));
-const priceHighest = (Math.max(...pricesJSON.map(Number)));
-const priceDiff = (priceHighest - priceLowest)/3;
-const priceAvg = pricesJSON.map(Number).reduce((a, b) => a + b, 0) / pricesJSON.length;
-
-async function createWidget(){
-  let listwidget = new ListWidget();
-  listwidget.backgroundColor = new Color("#000000");
-  let row = listwidget.addStack();
-  row.layoutVertically();
-  let left = row.addStack();
-  left.layoutHorizontally();
-  let whatday = left.addText(day);
-  whatday.textColor = new Color("#ffffff");
-  whatday.font = Font.lightSystemFont(20);
-  let right = left.addStack();
-  right.layoutVertically();
-  let update = right.addStack();
-  update.addSpacer();
-  let updatetext = update.addText(t("updated") + updated);
-  updatetext.font = Font.lightSystemFont(10);
-  updatetext.textColor = new Color("#ffffff");
-  let moms = right.addStack();
-  moms.addSpacer();
-  momstext = moms.addText("v. " + version);
-  momstext.font = Font.lightSystemFont(10);
-  momstext.textColor = new Color("#ffffff");
-  moms.addSpacer();
-  momstext = moms.addText(area);
-  momstext.font = Font.lightSystemFont(10);
-  momstext.textColor = new Color("#ffffff");
-  moms.addSpacer();
-  momstext = moms.addText("Extras: " + extras);
-  momstext.font = Font.lightSystemFont(10);
-  momstext.textColor = new Color("#ffffff");
-  moms.addSpacer();
-  if (includevat == 1) {
-    momstext = moms.addText(t("inclvat"));
-  }
-  else {
-    momstext = moms.addText(t("exclvat"));
-  }
-  momstext.font = Font.lightSystemFont(10);
-  momstext.textColor = new Color("#ffffff");
-  
+async function Table() {
   if (settings.showtable == "Yes"){
   width = 770;
   let head = listwidget.addStack()
@@ -551,9 +443,12 @@ for (let s = 0; s < stackNames.length; s++) {
   }
 }
   }
-  
-  
-  //chart
+
+
+}
+
+async function Graph() {
+//chart
   if (resolution == 60 && settings.showgraph == "Yes") {
     let avgtoday = []
     let dotNow = ""
@@ -623,8 +518,122 @@ for (let s = 0; s < stackNames.length; s++) {
     let chart = listwidget.addStack()
     chart.addImage(GRAPH) 
   }
- 
+}
+
+ async function PriceStats() {
+  let bottom = listwidget.addStack();
+  // lowest
+  let lowest = bottom.addText(t("lowest"));
+  lowest.font = Font.lightSystemFont(11);
+  lowest.textColor = new Color("#00cf00");
+  bottom.addSpacer(4);
+  let priceLowestRound = Math.round(priceLowest);
+  let lowesttext = bottom.addText(`${priceLowestRound}`);
+  lowesttext.font = Font.lightSystemFont(11);
+  lowesttext.textColor = new Color("#00cf00");
+  bottom.addSpacer();
+  // average
+  let avg = bottom.addText(t("average"));
+  avg.font = Font.lightSystemFont(11);
+  avg.textColor = new Color("#f38");
+  bottom.addSpacer(4);
+  let priceAvgRound = Math.round(priceAvg);
+  let avgtext = bottom.addText(`${priceAvgRound}`);
+  avgtext.font = Font.lightSystemFont(11);
+  avgtext.textColor = new Color("#f38");
+  bottom.addSpacer();
+  // highest
+  let highest = bottom.addText(t("highest"));
+  highest.font = Font.lightSystemFont(11);
+  highest.textColor = new Color("#fa60ff");
+  bottom.addSpacer(4);
+  let priceHighestRound = Math.round(priceHighest);
+  let highesttext = bottom.addText(`${priceHighestRound}`);
+  highesttext.font = Font.lightSystemFont(11);
+  highesttext.textColor = new Color("#fa60ff");
+  }
+
+
+const smallFont = 10;
+const mediumFont = 12;
+const bigFont = 13.5;
+const date = new Date();
+const yyyy = date.getFullYear();
+const mm = String(date.getMonth() + 1).padStart(2, '0'); // month are indexed from 0
+const dd = String(date.getDate()).padStart(2, '0');
+const formattedDate = `${yyyy}-${mm}-${dd}`;
+const hour = date.getHours();
+const minute = date.getMinutes();
+const hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+url = `https://dataportal-api.nordpoolgroup.com/api/DayAheadPriceIndices?date=${formattedDate}&market=DayAhead&indexNames=${area}&currency=${currency}&resolutionInMinutes=${resolution}`;
+const request = new Request(url);
+request.timeoutInterval = 1;
+let response = (await request.loadJSON());
+let updated = response.updatedAt;
+updated = updated.replace(/\.\d+Z$/, '').replace('T', ' ');
+const day = response.deliveryDateCET;
+let prices = response.multiIndexEntries;
+let allValues = [];
+
+for (let i = 0; i < prices.length; i++) {
+  const value = prices[i]["entryPerArea"][`${area}`];
+  allValues.push(String(value/10* (1 + "." + (includevat*vat)) + extras));
+}
+
+let pricesJSON = JSON.parse(JSON.stringify(allValues));
+// Test data for 15 min values
+//pricesJSON = ["53.7", "89.1", "60.2", "97.9", "70.8", "40.3", "48.6", "81.7", "26.4", "73.5","75.1", "39.7", "62.8", "18.5", "92.6", "33.1", "20.7", "11.4", "55.5", "46.9","85.0", "35.6", "79.2", "90.4", "66.7", "67.3", "28.8", "15.3", "99.6", "64.5","38.9", "57.2", "19.8", "71.6", "84.4", "49.5", "14.7", "63.1", "21.6", "44.2","78.5", "37.4", "17.2", "13.8", "12.6", "45.3", "58.6", "43.8", "16.9", "69.2","24.1", "41.6", "50.8", "36.3", "59.9", "95.4", "42.5", "93.7", "61.4", "27.5","47.7", "31.9", "32.8", "25.2", "83.6", "30.5", "74.2", "22.4", "77.1", "29.6","34.7", "52.1", "56.8", "23.3", "86.3", "65.4", "91.2", "68.4", "94.9", "98.5","76.3", "87.5", "88.7", "51.3", "80.1", "82.2", "72.7", "96.8", "87.0", "10.9","10.1", "10.4", "10.7", "11.9", "12.2", "13.5", "14.1", "15.8", "16.4", "17.7"]
+  
+const priceLowest = (Math.min(...pricesJSON.map(Number)));
+const priceHighest = (Math.max(...pricesJSON.map(Number)));
+const priceDiff = (priceHighest - priceLowest)/3;
+const priceAvg = pricesJSON.map(Number).reduce((a, b) => a + b, 0) / pricesJSON.length;
+
+let listwidget = new ListWidget();
+
+async function createWidget(){
+  listwidget.backgroundColor = new Color("#000000");
+  let row = listwidget.addStack();
+  row.layoutVertically();
+  let left = row.addStack();
+  left.layoutHorizontally();
+  let whatday = left.addText(day);
+  whatday.textColor = new Color("#ffffff");
+  whatday.font = Font.lightSystemFont(20);
+  let right = left.addStack();
+  right.layoutVertically();
+  let update = right.addStack();
+  update.addSpacer();
+  let updatetext = update.addText(t("updated") + updated);
+  updatetext.font = Font.lightSystemFont(10);
+  updatetext.textColor = new Color("#ffffff");
+  let moms = right.addStack();
+  moms.addSpacer();
+  momstext = moms.addText("v. " + version);
+  momstext.font = Font.lightSystemFont(10);
+  momstext.textColor = new Color("#ffffff");
+  moms.addSpacer();
+  momstext = moms.addText(area);
+  momstext.font = Font.lightSystemFont(10);
+  momstext.textColor = new Color("#ffffff");
+  moms.addSpacer();
+  momstext = moms.addText("Extras: " + extras);
+  momstext.font = Font.lightSystemFont(10);
+  momstext.textColor = new Color("#ffffff");
+  moms.addSpacer();
+  if (includevat == 1) {
+    momstext = moms.addText(t("inclvat"));
+  }
+  else {
+    momstext = moms.addText(t("exclvat"));
+  }
+  momstext.font = Font.lightSystemFont(10);
+  momstext.textColor = new Color("#ffffff");
+  
+
+  await Table();
   await PriceStats();
+  await Graph();
 return listwidget
 }
 

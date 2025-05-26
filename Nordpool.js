5607eccea00dfeda80548878280e72d08173ca7b
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-05-26 10:17:30 +0200

    Update Nordpool.js

--- Nordpool.js
+++ Nordpool.js
@@ -4,7 +4,7 @@
 // License: Personal use only. See LICENSE for details.
 // This script was created by Flopp999
 // Support me with a coffee https://www.buymeacoffee.com/flopp999 
-let version = 0.723
+let version = 0.724
 let allValues = [];
 let widget;
 let day;

1ff9638cb8aa73f39d5aa40cf7bc390b2c489016
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-06-07 20:35:26 +0200

    Update EiB.js

--- EiB.js
+++ EiB.js
@@ -4,7 +4,7 @@
 // License: Personal use only. See LICENSE for details.
 // This script was created by Flopp999
 // Support me with a coffee https://www.buymeacoffee.com/flopp999 
-let version = 0.20
+let version = 0.21
 const baseURL = "https://api.checkwatt.se";
 let password;
 let username;

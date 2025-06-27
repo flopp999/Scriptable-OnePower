0dc770aaac11dcf6fd92c3dd0960dd8cf2fc6466
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-06-27 21:15:42 +0200

    Update OnePowerWatt.js

--- OnePowerWatt.js
+++ OnePowerWatt.js
@@ -4,7 +4,7 @@
 // License: Personal use only. See LICENSE for details.
 // This script was created by Flopp999
 // Support me with a coffee https://www.buymeacoffee.com/flopp999 
-let version = 0.46
+let version = 0.47
 let widget;
 let day;
 let todaydate;
